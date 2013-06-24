2df62dd19531ce9a2874aa52f8149778a3eb1c81
Iñaki Abete <inakiabt@gmail.com>
2013-06-23 22:57:02 -0300

    Wrap log methods

--- src/Logger/MonologWrapper.php
+++ src/Logger/MonologWrapper.php
@@ -14,4 +14,199 @@ class MonologWrapper extends Logger
 	{
 		return $this->processors;
 	}
+
+
+    /**
+     * Adds a log record at an arbitrary level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  mixed   $level   The log level
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function log($level, $message, array $vars = array(), array $context = array())
+    {
+        return parent::log($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the DEBUG level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function debug($message, array $vars = array(), array $context = array())
+    {
+        return parent::debug($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the INFO level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function info($message, array $vars = array(), array $context = array())
+    {
+        return parent::info($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the INFO level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function notice($message, array $vars = array(), array $context = array())
+    {
+        return parent::notice($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the WARNING level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function warn($message, array $vars = array(), array $context = array())
+    {
+        return parent::warn($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the WARNING level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function warning($message, array $vars = array(), array $context = array())
+    {
+        return parent::warning($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the ERROR level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function err($message, array $vars = array(), array $context = array())
+    {
+        return parent::err($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the ERROR level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function error($message, array $vars = array(), array $context = array())
+    {
+        return parent::error($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the CRITICAL level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function crit($message, array $vars = array(), array $context = array())
+    {
+        return parent::crit($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the CRITICAL level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function critical($message, array $vars = array(), array $context = array())
+    {
+        return parent::critical($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the ALERT level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function alert($message, array $vars = array(), array $context = array())
+    {
+        return parent::alert($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the EMERGENCY level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function emerg($message, array $vars = array(), array $context = array())
+    {
+        return parent::emerg($this->format($message, $vars), $context);
+    }
+
+    /**
+     * Adds a log record at the EMERGENCY level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param  string  $message The log message
+     * @param  array   $context The log context
+     * @return Boolean Whether the record has been processed
+     */
+    public function emergency($message, array $vars = array(), array $context = array())
+    {
+        return parent::emergency($this->format($message, $vars), $context);
+    }
+
+    public function format($message, $args = array())
+    {
+        return preg_replace_callback(
+            '#{}#',
+            function () use (&$args) {
+                return (string) array_shift($args);
+            },
+            $message
+        );
+    }    
 }
